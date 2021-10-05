73f29990b3e1d6e5071b364661e27a4328390db1
Yosri Hammadi <yosri.hammadi@gmail.com>
2021-10-05 16:49:19 +0100

    fix bars width and removed logs

--- assets/js/quicksort.js
+++ assets/js/quicksort.js
@@ -1,59 +1,58 @@
-const quickButton = document.querySelector('.quick');
+const quickButton = document.querySelector(".quick");
 
-async function pivotHelper (array, start = 0, end = array.length - 1) {
-    let pivot = array[start];
-    let pivotIndex = start;
-    sizeHasChanged = false;
-    changeTwoElementColor(array[pivotIndex], null, 'purple')
+async function pivotHelper(array, start = 0, end = array.length - 1) {
+  let pivot = array[start];
+  let pivotIndex = start;
+  sizeHasChanged = false;
+  changeTwoElementColor(array[pivotIndex], null, "purple");
+  if (stats === 1) await pauser();
+  for (let i = start + 1; i <= end; i++) {
+    if (sizeHasChanged) return;
+    changeTwoElementColor(array[i], null, "#0081a7");
     if (stats === 1) await pauser();
-    for (let i=start + 1; i<=end; i++) {
-        if (sizeHasChanged) return;
-        changeTwoElementColor(array[i], null, '#0081a7')
-        if (stats === 1) await pauser();
-        if (array[i].value < pivot.value) {
-            pivotIndex++;
-            console.log(i, pivotIndex);
-            translateTwoElementSelection(array[pivotIndex], array[i], i - pivotIndex)
-            await pause(delay)
-            if (stats === 1) await pauser();
-            changeTwoElementColor(array[pivotIndex], array[i], '#95b27b')
-            if (stats === 1) await pauser();
-            swap(array, pivotIndex, i)
-        }
-        changeTwoElementColor(null, array[i], '#ee6c4d')
+    if (array[i].value < pivot.value) {
+      pivotIndex++;
+      translateTwoElementSelection(array[pivotIndex], array[i], i - pivotIndex);
+      await pause(delay);
+      if (stats === 1) await pauser();
+      changeTwoElementColor(array[pivotIndex], array[i], "#95b27b");
+      if (stats === 1) await pauser();
+      swap(array, pivotIndex, i);
     }
-    changeTwoElementColor(array[start], array[pivotIndex], '#95b27b')
-    translateTwoElementSelection(array[start], array[pivotIndex], pivotIndex - start)
-    await pause(delay)
-    if (stats === 1) await pauser();
-    changeTwoElementColor(array[start], array[pivotIndex], '#ee6c4d')
+    changeTwoElementColor(null, array[i], "#ee6c4d");
+  }
+  changeTwoElementColor(array[start], array[pivotIndex], "#95b27b");
+  translateTwoElementSelection(
+    array[start],
+    array[pivotIndex],
+    pivotIndex - start
+  );
+  await pause(delay);
+  if (stats === 1) await pauser();
+  changeTwoElementColor(array[start], array[pivotIndex], "#ee6c4d");
 
-    console.log(pivotIndex);
-    swap(array, start, pivotIndex);
-    return pivotIndex;
+  swap(array, start, pivotIndex);
+  return pivotIndex;
 }
 
-async function quickSort(arr, left = 0, right = arr.length -1){
-    if(left < right){
-        let pivotIndex = await pivotHelper(arr, left, right) //3
-        //left
-        if (sizeHasChanged) return;
-        await quickSort(arr,left,pivotIndex-1);
-        //right
-        if (sizeHasChanged) return;
-        await quickSort(arr,pivotIndex+1,right);
-      }
-     return arr;
-} 
+async function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = await pivotHelper(arr, left, right); //3
+    //left
+    if (sizeHasChanged) return;
+    await quickSort(arr, left, pivotIndex - 1);
+    //right
+    if (sizeHasChanged) return;
+    await quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
 
 async function quickSortHelper() {
-    const elements = document.querySelectorAll('.array-element');
-    const arrayOfObject = createArrayOfObject(elements);
-    await quickSort(arrayOfObject);
-
+  const elements = document.querySelectorAll(".array-element");
+  const arrayOfObject = createArrayOfObject(elements);
+  await quickSort(arrayOfObject);
 }
-
-
 
 quickButton.addEventListener("click", async function () {
   const attr = document.createAttribute("highlight");
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-const delayElement = document.getElementById('delay');
-const sizeElement = document.getElementById('size');
-const visualizer = document.querySelector('.visualizer');
-const playButton = document.querySelector('.play');
-const pauseButton = document.querySelector('.pause');
-const refreshButton = document.querySelector('.refresh');
+const delayElement = document.getElementById("delay");
+const sizeElement = document.getElementById("size");
+const visualizer = document.querySelector(".visualizer");
+const playButton = document.querySelector(".play");
+const pauseButton = document.querySelector(".pause");
+const refreshButton = document.querySelector(".refresh");
 let totalWidth = 720;
 let arrayToSort = [];
 let delay = 500;
@@ -14,162 +14,161 @@ delayElement.value = 1000 - delay;
 sizeElement.value = arrayLen;
 let elementWidth = calculateElementWidth();
 
-
 function GetElementWidth() {
-    console.log(window.innerWidth)
-    if (window.innerWidth > 740) {
-        totalWidth = 720;
-        elementWidth = calculateElementWidth();
-        generateArray(arrayLen);
-    } 
-    else if (window.innerWidth <= 340) {
-        totalWidth = 200;
-        elementWidth = calculateElementWidth();
-        generateArray(arrayLen);
-    }
-    else if (window.innerWidth <= 550) {
-        totalWidth = 320;
-        elementWidth = calculateElementWidth();
-        generateArray(arrayLen);
-    }
-    else if (window.innerWidth <= 740) {
-        totalWidth = 500;
-        elementWidth = calculateElementWidth();
-        generateArray(arrayLen);
-    }
-}
-
-window.addEventListener('resize', GetElementWidth);
-
-delayElement.oninput = () => {
-    delay = 1000 - delayElement.value
-}
-
-sizeElement.oninput = () => {
-    arrayLen = sizeElement.value;
+  if (window.innerWidth > 740) {
+    totalWidth = 720;
     elementWidth = calculateElementWidth();
     generateArray(arrayLen);
-    sizeHasChanged = true;
+  } else if (window.innerWidth <= 340) {
+    totalWidth = 200;
+    elementWidth = calculateElementWidth();
+    generateArray(arrayLen);
+  } else if (window.innerWidth <= 550) {
+    totalWidth = 320;
+    elementWidth = calculateElementWidth();
+    generateArray(arrayLen);
+  } else if (window.innerWidth <= 740) {
+    totalWidth = 500;
+    elementWidth = calculateElementWidth();
+    generateArray(arrayLen);
+  }
 }
+
+window.addEventListener("resize", GetElementWidth);
+
+delayElement.oninput = () => {
+  delay = 1000 - delayElement.value;
+};
+
+sizeElement.oninput = () => {
+  arrayLen = sizeElement.value;
+  elementWidth = calculateElementWidth();
+  generateArray(arrayLen);
+  sizeHasChanged = true;
+};
 
 function resetSettings() {
-    arrayToSort = [];
-    visualizer.innerHTML = '';
-    stats = 0;
-    sizeHasChanged = true;
-
+  arrayToSort = [];
+  visualizer.innerHTML = "";
+  stats = 0;
+  sizeHasChanged = true;
 }
 
-function calculateElementWidth () {
-    return (totalWidth / arrayLen) / 2;
- }
+function calculateElementWidth() {
+  const elementWidth = Math.floor(totalWidth / arrayLen / 2);
+  //   const elementWidth = totalWidth / arrayLen / 2;
+  return elementWidth;
+}
 
 function generateArray(len) {
-    resetSettings()
-    for (let i = 0; i < len; i++) {
-        const rand = Math.floor(Math.random() * 100) + 1
-        arrayToSort.push(rand);
-        createArrayElement(rand);
-    }
+  resetSettings();
+  for (let i = 0; i < len; i++) {
+    const rand = Math.floor(Math.random() * 100) + 1;
+    arrayToSort.push(rand);
+    createArrayElement(rand);
+  }
 }
 
 function createArrayElement(val) {
-    // visualizer.style.gap = elementWidth + 'px';
-    const arrayElement = document.createElement('div');
-    arrayElement.className = 'array-element';
-    arrayElement.style.marginLeft = (elementWidth/2)+ 'px';
-    arrayElement.style.marginRight = (elementWidth/2) + 'px';
-    arrayElement.style.height = val + '%';
-    arrayElement.style.width = elementWidth + 'px';
-    // arrayElement.innerText = val
-    visualizer.appendChild(arrayElement);
+  // visualizer.style.gap = elementWidth + 'px';
+  const arrayElement = document.createElement("div");
+  arrayElement.className = "array-element";
+  arrayElement.style.marginLeft = elementWidth / 2 + "px";
+  arrayElement.style.marginRight = elementWidth / 2 + "px";
+  arrayElement.style.height = val + "%";
+  arrayElement.style.width = elementWidth + "px";
+  // arrayElement.innerText = val
+  visualizer.appendChild(arrayElement);
 }
 
 function createArrayOfObject(elements) {
-    const arrayOfObject = [];
-    for (let i=0; i < elements.length; i++) {
-        const object = {'element': elements[i], 'translateValue': 0, 'value' : arrayToSort[i]}
-        arrayOfObject.push(object);
-    }
-    return arrayOfObject;
+  const arrayOfObject = [];
+  for (let i = 0; i < elements.length; i++) {
+    const object = {
+      element: elements[i],
+      translateValue: 0,
+      value: arrayToSort[i],
+    };
+    arrayOfObject.push(object);
+  }
+  return arrayOfObject;
 }
 
 function changeTwoElementColor(firstElement, secondElement, color) {
-    if (firstElement !== null) {
-        firstElement.element.style.backgroundColor = color;
-    }
-    if (secondElement !== null) {
-        secondElement.element.style.backgroundColor = color;
-    }
+  if (firstElement !== null) {
+    firstElement.element.style.backgroundColor = color;
+  }
+  if (secondElement !== null) {
+    secondElement.element.style.backgroundColor = color;
+  }
 }
 
 function translateTwoElement(firstElement, secondElement) {
-    firstElement.translateValue = firstElement.translateValue + (elementWidth*2);
-    secondElement.translateValue = secondElement.translateValue - (elementWidth*2);
-    const x = firstElement.translateValue;
-    const y = secondElement.translateValue;
-    firstElement.element.style.transform = `translate(${x}px)`
-    secondElement.element.style.transform = `translate(${y}px)`
+  firstElement.translateValue = firstElement.translateValue + elementWidth * 2;
+  secondElement.translateValue =
+    secondElement.translateValue - elementWidth * 2;
+  const x = firstElement.translateValue;
+  const y = secondElement.translateValue;
+  firstElement.element.style.transform = `translate(${x}px)`;
+  secondElement.element.style.transform = `translate(${y}px)`;
 }
 
 function translateTwoElementSelection(firstElement, secondElement, distance) {
-    firstElement.translateValue = firstElement.translateValue + (elementWidth*2) * distance;
-    secondElement.translateValue = secondElement.translateValue - (elementWidth*2) * distance;
-    const x = firstElement.translateValue;
-    const y = secondElement.translateValue;
-    firstElement.element.style.transform = `translate(${x}px)`
-    secondElement.element.style.transform = `translate(${y}px)`
-
+  firstElement.translateValue =
+    firstElement.translateValue + elementWidth * 2 * distance;
+  secondElement.translateValue =
+    secondElement.translateValue - elementWidth * 2 * distance;
+  const x = firstElement.translateValue;
+  const y = secondElement.translateValue;
+  firstElement.element.style.transform = `translate(${x}px)`;
+  secondElement.element.style.transform = `translate(${y}px)`;
 }
 
-function swap (arr, i, j) {
-    const temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
+function swap(arr, i, j) {
+  const temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
 }
 
-const pause = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
+const pause = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 function pauser() {
-    return new Promise(resolve => {
-        let playButtonClick = function() {
-            playButton.removeEventListener('click', playButtonClick);
-            stats = 0;
-            resolve('resolved');
-        }
-        playButton.addEventListener('click', playButtonClick);
-    })
+  return new Promise((resolve) => {
+    let playButtonClick = function () {
+      playButton.removeEventListener("click", playButtonClick);
+      stats = 0;
+      resolve("resolved");
+    };
+    playButton.addEventListener("click", playButtonClick);
+  });
 }
-pauseButton.addEventListener('click', function() {
-    stats = 1;
-})
-refreshButton.addEventListener('click',() => {
-    generateArray(arrayLen);
+pauseButton.addEventListener("click", function () {
+  stats = 1;
+});
+refreshButton.addEventListener("click", () => {
+  generateArray(arrayLen);
 });
 
-function translateHelper (element, firstIndex, lastIndex) {
-    element.translateValue += elementWidth * 2 * (firstIndex - lastIndex);
-    const x = element.translateValue;
-    element.element.style.transform = `translateX(${x}px)`;
+function translateHelper(element, firstIndex, lastIndex) {
+  element.translateValue += elementWidth * 2 * (firstIndex - lastIndex);
+  const x = element.translateValue;
+  element.element.style.transform = `translateX(${x}px)`;
 }
 
 function changeColorHelper(arr, color) {
-    for (let i = 0; i < arr.length ; i++) {
-        arr[i].element.style.backgroundColor = "#" + color;
-    }
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].element.style.backgroundColor = "#" + color;
+  }
 }
 
-window.addEventListener('load', (event) => {
-    GetElementWidth();
-    generateArray(arrayLen);
-//     console.log(arrayToSort);
-// selectionSort()
-// console.log(arrayToSort);
-})
+window.addEventListener("load", (event) => {
+  GetElementWidth();
+  generateArray(arrayLen);
+});
 
-const getButtons = document.querySelectorAll('.btn-algo');
+const getButtons = document.querySelectorAll(".btn-algo");
 
-function disableButtons( isDisabled ) {
-    for (let button of getButtons) {
-        button.disabled = isDisabled;
-    }
+function disableButtons(isDisabled) {
+  for (let button of getButtons) {
+    button.disabled = isDisabled;
+  }
 }
